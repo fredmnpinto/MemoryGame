a8f31417ff15be9c621db27cd0a71d76962474b7
Frederico Pinto <39822@gmail.com>
2020-06-10 20:43:54 +0100

    First commit
    Basicamente comecei o html, o javascript e o json (incompleto).

---
+++ data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "What does HTML stand for?",
+        "opt1" : "Hyper Text Markup Language",
+        "opt2" : "Hyper Texting Makeup Laxative",
+        "opt3" : "Humongous Text Made by Larry",
+        "answer" : "Hyper Text Markup Language"
+    },
+    {
+        "q" : "Mark the right sentences:",
+        "opt1" : "<P> is used to start a new paragraph in HTML",
+        "opt2" : "/op is a command that gives you admin privileges in Javascript",
+        "opt3" : "'write less, do more' is a common way to describe JQuery",
+        "answer" : ""
+    },
+    {
+        "q" : "",
+        "opt1" : "",
+        "opt2" : "",
+        "opt3" : "",
+        "answer" : ""
+    },
+    {
+        "q" : "",
+        "opt1" : "",
+        "opt2" : "",
+        "opt3" : "",
+        "answer" : ""
+    },
+    {
+        "q" : "",
+        "opt1" : "",
+        "opt2" : "",
+        "opt3" : "",
+        "answer" : ""
+    },
+]
