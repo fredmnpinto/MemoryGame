cbf99566eebe81f03fa916f25e8a950c7e998d38
Frederico Pinto <39822@gmail.com>
2020-06-10 21:03:18 +0100

    FirstCommit.2
    Faltou esse, eu imagino

---
+++ indx.js
@@ -0,0 +1,11 @@
+function generate(indx){
+    document.getElementById("question").innerHTML = jsonData[indx];
+    document.getElementById("qtype").innerHTML = jsonData[indx];
+    document.getElementById("opt1").innerHTML = jsonData[indx];
+    document.getElementById("opt2").innerHTML = jsonData[indx];
+    document.getElementById("opt3").innerHTML = jsonData[indx];
+}
+
+function checarResposta(){
+    if(document.getElementById.checado && )
+};
