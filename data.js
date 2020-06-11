e65f5caf8762cb01211d1aaf94a13b30eb81ba8e
Frederico Pinto <39822@gmail.com>
2020-06-11 18:01:18 +0100

    Commit 2
    Fiz a funcao checarRespostaUnica, que checa se a resposta a pergutna de escolha unica esta correta e dei continuidade a outros parametros
    NOTA: erro a consertar [HTML nao mostrando a resposta]

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 var jsonData = [
     {
         "q" : "What does HTML stand for?",
+        "qtype" : "EscolhaUnica",
         "opt1" : "Hyper Text Markup Language",
         "opt2" : "Hyper Texting Makeup Laxative",
         "opt3" : "Humongous Text Made by Larry",
@@ -8,30 +9,28 @@ var jsonData = [
     },
     {
         "q" : "Mark the right sentences:",
+        "qtype" : "EscolhaMultipla",
         "opt1" : "<P> is used to start a new paragraph in HTML",
         "opt2" : "/op is a command that gives you admin privileges in Javascript",
         "opt3" : "'write less, do more' is a common way to describe JQuery",
         "answer" : ""
     },
     {
-        "q" : "",
-        "opt1" : "",
-        "opt2" : "",
-        "opt3" : "",
-        "answer" : ""
+        "q" : "Mark a C for correct and F for false on the following sentences:",
+        "qtype" : "VerdadeiroFalso",
+        "opt1" : "Javascript better than C",
+        "opt2" : "2020 has been great so far",
+        "opt3" : "The mitochondria is the powerhouse of the cell",
+        "answer" : "FFC"
     },
     {
-        "q" : "",
-        "opt1" : "",
-        "opt2" : "",
-        "opt3" : "",
-        "answer" : ""
-    },
-    {
-        "q" : "",
-        "opt1" : "",
-        "opt2" : "",
-        "opt3" : "",
-        "answer" : ""
-    },
-]
+        "q" : "The english language is currently the ______ human language.",
+        "qtype" : "PreencherEspaco",
+        "opt1" : "NULL", // Ainda nao sei como vou organizar essa pergunta nesse formato
+        "opt2" : "NULL",
+        "opt3" : "NULL",
+        "answer" : "universal"
+
+        
+    }
+];
--- indx.js
+++ indx.js
@@ -6,6 +6,36 @@ function generate(indx){
     document.getElementById("opt3").innerHTML = jsonData[indx];
 }
 
+var i = 0; //questao em que esta
+var pontuacao = 0;
 function checarResposta(){
-    if(document.getElementById.checado && )
+    switch(jsonData[i].qtype){
+        case "EscolhaUnica" : checarRespostaUnica; break;
+        case "EscolhaMultipla" : break;
+        case "VerdadeiroFalso" : break;
+        case "PreencherEspaco" : break;
+        default : alert("ERROR : 'qtype' nao previsto.");
+    }
+    i++;
+    if(jsonData.length - 1 < i){
+        document.write("Você acertou " +pontuacao+ " questoes.");
+        if(pontuacao == 4) document.write("<br>Perfeito! Parabéns!");
+        else if(pontuacao == 1 || pontuacao == 0) document.write("<br>Hmm mais sorte na proxima...");
+    }
+}
+function checarRespostaUnica(){
+
+    if(document.getElementById("opt1").checado && jsonData[i].opt1 == jsonData[i].answer){
+        pontuacao++;
+        alert("Correto!");
+    }
+    if(document.getElementById("opt2").checado && jsonData[i].opt2 == jsonData[i].answer){
+        pontuacao++;
+        alert("Correto!");
+    }
+    if(document.getElementById("opt3").checado && jsonData[i].opt3 == jsonData[i].answer){
+        pontuacao++;
+        alert("Correto!");
+    }
+   
 };
